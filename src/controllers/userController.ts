23dbc1465f8e795f7be6321cfa370681d8ed0f94
Daniel Georgiev <daniel.georgiev01@sap.com>
2022-04-11 13:12:34 +0300

    expose error messages

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -10,41 +10,50 @@ export class UserController {
   constructor() {
     DbService.createDb();
   }
+
   public async registerUser(req: Request, res: Response) {
-    const hashedPassword = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    try {
+      const hashedPassword = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 
-    const user: User = {
-      username: req.body.username,
-      password: hashedPassword,
+      const user: User = {
+        username: req.body.username,
+        password: hashedPassword,
 
-    };
-    
-    DbService.createUser(user);
+      };
 
-    const token = jwt.sign({ username: req.body.username, scope : req.body.scope }, process.env["JWT_SECRET"]);
-    return res.status(200).send({ token: token });
+      DbService.createUser(user);
+
+      const token = jwt.sign({ username: req.body.username, scope: req.body.scope }, process.env["JWT_SECRET"]);
+      return res.status(200).send({ token: token });
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
   }
 
   public async loginUser(req: Request, res: Response) {
-    const { username, password } = req.body;
-    if (!(username && password)) {
-      return res.status(400).send("All input is required");
-    }
-
-    const userResult = await DbService.findUserByUsername(req.body.username);
-    const user = userResult.rows[0];
-    if (!user) {
-      return res.status(401).json({ status: "error", code: "unauthorized" });
-    }
-    
-    bcrypt.compare(password, user.password, function (err, result) {
-      if (result) {
-        const token = jwt.sign({ username: req.body.username, scope: req.body.scope }, process.env["JWT_SECRET"]);
-        return res.status(200).send({ token: token });
+    try {
+      const { username, password } = req.body;
+      if (!(username && password)) {
+        return res.status(400).send("All input is required");
       }
 
-      return res.status(401).json({ status: "error", code: "unauthorized" });
-    });
+      const userResult = await DbService.findUserByUsername(req.body.username);
+      const user = userResult.rows[0];
+      if (!user) {
+        return res.status(401).json({ status: "error", code: "unauthorized" });
+      }
+
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result) {
+          const token = jwt.sign({ username: req.body.username, scope: req.body.scope }, process.env["JWT_SECRET"]);
+          return res.status(200).send({ token: token });
+        }
+
+        return res.status(401).json({ status: "error", code: "unauthorized" });
+      });
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
   }
 
   public async loadItems(req: Request, res: Response){
@@ -54,7 +63,7 @@ export class UserController {
       console.log(items);
       return res.status(200).send({ items: items, message: "Items are loaded." });
     } catch (error) {
-      return res.status(500).send("Server error.");
+      return res.status(500).send({message: error.message});
     }
   }
 
@@ -68,7 +77,7 @@ export class UserController {
       console.log(item);
       return res.status(200).send({ data: item });
     } catch (error) {
-      return res.status(500).send("Server error.");
+      return res.status(500).send({message: error.message});
     }
   }
 
@@ -79,7 +88,7 @@ export class UserController {
       console.log(`Item id=${id} deleted.`);
       return res.status(200).send({message: "Item deleted."});
     } catch (error) {
-      return res.status(500).send("Server error.");
+      return res.status(500).send({message: error.message});
     }
   }
 
@@ -89,22 +98,21 @@ export class UserController {
       await DbService.updateById(newItem);
       return res.status(200).send({message: "Item is updated."});
     } catch (error) {
-      return res.status(500).send("Server error.");
+      return res.status(500).send({message: error.message});
     }
   }
 
   public async heavyTask(req: Request, res: Response) {
-
     try {
       const child = fork("src/tasks/heavyTask.ts");
 
       child.on("close", function (code) {
         console.log("child process exited with code " + code);
-        return res.status(200).send({message:"Long Running Operation Done!"});
+        return res.status(200).send({ message: "Long Running Operation Done!" });
       });
-      
+
     } catch (error) {
-      return res.status(500).send("Server error.");
+      return res.status(500).send({ message: error.message });
     }
   }
 }
